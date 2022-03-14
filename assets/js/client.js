a8ef5a050bf7287fd21e14a1595c7c624169fb05
YJyamashita <yuji.y125@gmail.com>
2022-03-14 22:45:35 +0900

    12

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,3 +1,17 @@
+var connection = new WebSocket('ws://localhost:9090');
+
+connection.onopen = function () {
+	console.log("Connected to the server");
+}
+
+connection.onmessage = function (msg) {
+	var data = JSON.parse(msg.data)
+}
+connection.onerror = function (error) {
+	console.log(error);
+}
+
+
 var url_string = window.location.href;
 var url = new URL(url_string);
 var username = url.searchParams.get("username");
